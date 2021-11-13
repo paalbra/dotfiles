05941bbada883c603f1a7f0c6f9d7554ae87e66b
Paal Braathen <paalbra@gmail.com>
2021-11-13 16:05:17 +0100

    Rewrite and store config

--- install.sh
+++ install.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 
+declare -A CONFIG
+CONFIGFILE="config.sh"
+
+function getvar() {
+    if [ ! -z "${CONFIG[$1]}" ]; then
+        echo "${CONFIG[$1]}"
+    else
+        echo "${2-Type in $1}:" >&2
+        read CONFIG[$1]
+        echo "${CONFIG[$1]}"
+    fi
+}
+
+function dumpvars() {
+    echo -n > "$1"
+    for key in "${!CONFIG[@]}"; do
+        echo "CONFIG[$key]='${CONFIG[$key]}'" >> "$1"
+    done
+}
+
+if [ -f "$CONFIGFILE" ]; then
+    source "$CONFIGFILE"
+fi
+
 REPO_DIR=$(pwd -P)
 
-FILES=$(find $REPO_DIR/files -maxdepth 1 -type f -name ".*" | sed -n "s|^$REPO_DIR/files/||p")
-BACKUP_DIR=$REPO_DIR/backup_$(date +%Y%m%d-%H%M%S)
-mkdir -p $BACKUP_DIR
+pushd ./files > /dev/null
+readarray -d '' FILES < <(find . -maxdepth 1 -type f -name ".*" -print0)
+popd > /dev/null
+BACKUP_DIR="$REPO_DIR/backup_$(date +%Y%m%d-%H%M%S)"
+mkdir -p "$BACKUP_DIR"
 
 echo "Installing dotfiles (old files are moved to $BACKUP_DIR)."
 
 # Copy all the files
-for FILE in $FILES
-do
-	if [ -f ~/$FILE ]; then
-		mv -v -i ~/$FILE $BACKUP_DIR
-	fi
-	cp -v -i files/$FILE ~/
+for FILE in "${FILES[@]}"; do
+    SRC=$(realpath "files/$FILE")
+    DST=$(realpath "$HOME/$FILE")
+    if ! cmp --quiet "$SRC" "$DST"; then
+        if [ -f "$DST" ]; then
+            mv -v -i "$DST" "$BACKUP_DIR"
+        fi
+        cp -v -i "$SRC" "$DST"
+    fi
 done
 
 echo "Configuring variables."
 
-# Replace variables
-echo "-- What name do you want to use for git?"
-read git_user_name
+getvar git_user_name "What name do you want to use for git?" > /dev/null
+getvar git_user_email "What email do you want to use for git?" > /dev/null
 
-echo "-- What email do you want to use for git?"
-read git_user_email
-
-sed -i "s/{git_user_name}/$git_user_name/g" ~/.gitconfig
-sed -i "s/{git_user_email}/$git_user_email/g" ~/.gitconfig
+sed -i "s/{git_user_name}/${CONFIG[git_user_name]}/g" ~/.gitconfig
+sed -i "s/{git_user_email}/${CONFIG[git_user_email]}/g" ~/.gitconfig
 
 echo "Configuring variables done."
+
+dumpvars "$CONFIGFILE"
 
 echo "Done!"
